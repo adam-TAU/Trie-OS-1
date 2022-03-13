1cde24abb1d3d1b232218a06897ecebcd7341cfc
adam <adam@adam.adam>
2022-03-13 11:29:53 +0200

    some design updates

--- pt.c
+++ pt.c
@@ -17,11 +17,11 @@
 
 /**************** AUXILIARY FUNCTION DECLARATIONS ******************/
 
-/* Given a page table's pointer, a VPN, and its desired (valid (!= NO_MAPPING)) PPN mapping, insert the mapping into the page table */
+/* Given a page table's pointer, a VPN, and its desired valid PPN mapping, insert the mapping into the page table */
 static void insert_mapping(uint64_t pt, uint64_t vpn, uint64_t ppn);
 
 /* Removes a page table mapping, if it exists. In case it does exist, it also cares to free the nodes that held the entries of the VPN's search path in the Page Table's,
- * if nodes were emptied due to the removal of the mapping - this doesn't apply to the root node of the Page Table tho. */
+ * if nodes were invalidated due to the removal of the mapping - this doesn't apply to the root node of the Page Table tho. */
 static bool remove_mapping(uint64_t *current_node_ppn, uint64_t vpn, size_t depth);
 
 
@@ -34,15 +34,24 @@ static bool remove_mapping(uint64_t *current_node_ppn, uint64_t vpn, size_t dept
  */
 static uint64_t* get_entry(uint64_t vpn, uint64_t ppn, size_t depth);
 
+/* Given an entry's pointer in a Page Table, invalidate the entry (by making the entry's value equal to zero, and thus making the valid bit 0 => invalid) */
+static void invalidate(uint64_t *ppn) {
+	*ppn = 0;
+}
+
+/* Determine if an entry inside a node maps to an invalid PPN - using its valid bit as an indicator.
+ * This corresponds with the NO_MAPPING value. */
+static bool is_valid(uint64_t ppn);
+
 /* Given a node's ppn, determine of its empty of any VPN's mapping. */
 static bool is_node_empty(uint64_t node_ppn);
 
 /* Given a node's ppn, free its Physical Page*/
 static void free_node(uint64_t node_ppn);
 
-/* If a node is empty of any VPN's mapping, free its Physical Page, and make sure to change the node's entry in the Page Table to NO_MAPPING.
+/* If a node is empty of any VPN's mapping, free its Physical Page, and make sure to invalidate the node's entry in the Page Table.
  * uint64_t *node_ppn: a pointer to the node's entry in the Page Table.
- * This function returns true IF the node was in fact emptied and freed, ELSE, it returns false. */
+ * This function returns true IF the node was in fact invalidated and freed, ELSE, it returns false. */
 static bool free_node_safe(uint64_t *node_ppn);
 
 
@@ -66,7 +75,7 @@ static void insert_mapping(uint64_t pt, uint64_t vpn, uint64_t ppn) {
 		} else { // we're still inside the search path
 		
 			// the entry of the VPN in the current node isn't mapped to a new node -> create a new node 
-			if (*entry == NO_MAPPING) *entry = alloc_page_frame(); 
+			if (is_valid(*entry) == false) *entry = alloc_page_frame(); 
 			
 			// continue to the next node in the search path				
 			current_node_ppn = *entry;
@@ -77,39 +86,39 @@ static void insert_mapping(uint64_t pt, uint64_t vpn, uint64_t ppn) {
 
 
 /* This recursive function will return true, if the current node that we're at in the recursion
- * has had any of its entried emptied (Nullified (= NO_MAPPING)) */
+ * has had any of its entried invalidate */
 static bool remove_mapping(uint64_t *current_node_ppn, uint64_t vpn, size_t depth) {
 	uint64_t* entry = get_entry(vpn, *current_node_ppn, depth);
 	
 	/* If we reached the end of the search path:
-	 * either one of the nodes in the search path is empty (*entry == NO_MAPPING)
+	 * either one of the nodes in the search path is empty (is_valid(*entry) == false)
 	 * or this is the last node in the search path (depth == 4)
-	 * In both cases, nullify (= NO_MAPPING) the entry.
+	 * In both cases, invalidate the entry.
 	 * IF the entry was empty to begin with:
 	 		return false to signify to our parent node in the recursion,
-	 		that he can terminate the recursion since none of its entries were emptied
+	 		that he can terminate the recursion since none of its entries were invalidated
 	 * ELSE:
-	 		return true to signify to our next parent that he had one of its entried emptied.
+	 		return true to signify to our next parent that he had one of its entried invalidated.
 	 */
-	if (depth == 4 || (*entry == NO_MAPPING) ) {
+	if (depth == 4 || (is_valid(*entry) == false) ) {
 		
 		if (depth != 4) { // in this case, there was nothing to remove to begin with
 			return false;
-		} else if (*entry == NO_MAPPING) { // in this case, the entry of the VPN->PPN was empty to begin with
+		} else if (is_valid(*entry) == false) { // in this case, the entry of the VPN->PPN was empty to begin with
 			return true;
-		} else { // in this case, there was in fact a mapping that VPN held, and we'll nullify it as desired
-			*entry = NO_MAPPING;
+		} else { // in this case, there was in fact a mapping that VPN held, and we'll invalidate it as desired
+			invalidate(entry);
 			return true;
 		}
 
 	/* if we still need to advance in the search for the entry, simply advance to the next node in the search path */
 	} else { 
-		/* IF the recursion returned that the node that we're at hasn't had any entries emptied:
+		/* IF the recursion returned that the node that we're at hasn't had any entries invalidated:
 		 		we return false to signify to our parent node in the recursion,
-	 			that he can terminate the recursion since none of its entries were emptied
+	 			that he can terminate the recursion since none of its entries were invalidated
 		 * ELSE
 		 		we continue on to the next step,
-		 		of determining if the node we're at needs to be freed due to any of its entries being emptied,
+		 		of determining if the node we're at needs to be freed due to any of its entries being invalidated,
 		 		which is guaranteed due to <true> being returned.
 		 */
 		if (remove_mapping(entry, vpn, depth + 1) == false) {
@@ -120,11 +129,11 @@ static bool remove_mapping(uint64_t *current_node_ppn, uint64_t vpn, size_t dept
 	/* if we aren't in the page table's root node (which shouldn't be freed at all costs):
 	 * 		check if the node is empty of mapping:
 	 *			 IF so:
-	 				 it frees it, and changes the entry that held the PPN of the node to NO_MAPPING.
-	 				 Then, we return true to signify to our next parent that he had one of its entried emptied.
+	 				 it frees it, and changes the entry that held the PPN of the node to an invalid entry (valid bit == 0).
+	 				 Then, we return true to signify to our next parent that he had one of its entried invalidated.
 	 * 			 ELSE:
 	 				 the current node wasn't freed, and we return false to signify to our parent node in the recursion,
-	 				 that he can terminate the recursion since none of its entries were emptied. 
+	 				 that he can terminate the recursion since none of its entries were invalidated. 
 	 */
 	if (depth != 0) {
 		return free_node_safe(current_node_ppn);
@@ -158,6 +167,11 @@ static uint64_t* get_entry(uint64_t vpn, uint64_t node_ppn, size_t depth) {
 }
 
 
+static bool is_valid(uint64_t ppn) {
+	return ppn & 0x1;
+}
+
+
 static bool is_node_empty(uint64_t node_ppn) {
 	
 	/* DETERMINE IF ALL ENTRIES ARE EMPTY */
@@ -165,7 +179,7 @@ static bool is_node_empty(uint64_t node_ppn) {
 	bool empty = true; // assume the node has all empty entries
 	
 	for (size_t i = 0; i < 512; i++) {
-		if (node_ppn_virt[i] != NO_MAPPING) { // if there's an entry that isn't empty, the node isn't empty 
+		if (is_valid(node_ppn_virt[i])) { // if there's an entry that isn't empty, the node isn't empty 
 			empty = false;
 		}
 	}
@@ -182,7 +196,7 @@ static void free_node(uint64_t node_ppn) {
 static bool free_node_safe(uint64_t *node_ppn) {
 	if (is_node_empty(*node_ppn) == true) {
 		free_node(*node_ppn);
-		*node_ppn = NO_MAPPING;
+		invalidate(node_ppn);
 		return true;
 	}
 	
@@ -195,7 +209,7 @@ static bool free_node_safe(uint64_t *node_ppn) {
 
 void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn) {
 	
-	if (ppn != NO_MAPPING) { // if the desired action was to create/update the mapping
+	if (is_valid(ppn) == true) { // if the desired action was to create/update the mapping
 		insert_mapping(pt, vpn, ppn);
 	} else { // if the desired action was to remove the mapping
 		remove_mapping(&pt, vpn, 0);
@@ -212,9 +226,14 @@ uint64_t page_table_query(uint64_t pt, uint64_t vpn) {
 	for (size_t depth = 0; depth < 5; depth++) { /* Personal calculations showed that the Page Table should be of height 5, with 512 entries for each node */
 		uint64_t* entry = get_entry(vpn, current_node_ppn, depth);
 		printf("here1\n");
-		if (depth == 4 || (*entry == NO_MAPPING) ) { // we reached the end of the search path - we'll return the corresponding entry
+		if (depth == 4 || (!is_valid(*entry)) ) { // we reached the end of the search path - we'll return the corresponding entry
 			printf("here %li, should've been: %lli\n", *entry, NO_MAPPING);
-			return *entry;
+			
+			if (is_valid(*entry) == true) {
+				return *entry;
+			} else {
+				return NO_MAPPING;
+			}
 			
 		} else { // we're still inside the search path, continue to the next node in the search path	
 			current_node_ppn = *entry;
