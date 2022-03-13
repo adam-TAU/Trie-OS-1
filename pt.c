1e5b32c58c251d2755559249f2b6a3d5b9ee8b57
adam <adam@adam.adam>
2022-03-13 06:22:43 +0200

    changed some design and added documentation

--- pt.c
+++ pt.c
@@ -3,6 +3,8 @@
 #define bool int
 #define true 1
 #define false 0
+
+
 /* Submitter: Adam
  * Operating Systems, Tel Aviv University, 2022A.
  * ==============================================
@@ -38,12 +40,17 @@ static bool is_node_empty(uint64_t node_ppn);
 static void free_node(uint64_t node_ppn);
 
 /* If a node is empty of any VPN's mapping, free its Physical Page, and make sure to change the node's entry in the Page Table to NO_MAPPING.
- * uint64_t *node_ppn: a pointer to the node's entry in the Page Table. */
-static void free_node_safe(uint64_t *node_ppn);
+ * uint64_t *node_ppn: a pointer to the node's entry in the Page Table.
+ * This function returns true IF the node was in fact emptied and freed, ELSE, it returns false. */
+static bool free_node_safe(uint64_t *node_ppn);
+
+
+
+
+
 
 
 /**************** AUXILIARY FUNCTION DEFINITIONS ******************/
-
 
 static void insert_mapping(uint64_t pt, uint64_t vpn, uint64_t ppn) {
 
@@ -68,25 +75,61 @@ static void insert_mapping(uint64_t pt, uint64_t vpn, uint64_t ppn) {
 }
 
 
-
+/* This recursive function will return true, if the current node that we're at in the recursion
+ * has had any of its entried emptied (Nullified (= NO_MAPPING)) */
 static bool remove_mapping(uint64_t *current_node_ppn, uint64_t vpn, size_t depth) {
 	uint64_t* entry = get_entry(vpn, *current_node_ppn, depth);
 	
-	if (depth == 4 || (*entry == NO_MAPPING) ) { // if we reached the end of the path (either one of the entries in the path is empty (*entry == NO_MAPPING), or if this is the last node in the path (depth == 4)), nullify (= NO_MAPPING) the entry 
-		*entry = NO_MAPPING;
-		if (depth != 4) return false;
-		else return true;
-	} else { // if we still need to advance in the search for the entry, simply advance to the next node in the path
+	/* if we reached the end of the path:
+	 * either one of the entries in the path is empty (*entry == NO_MAPPING)
+	 * or this is the last node in the path (depth == 4)
+	 * In both cases, nullify (= NO_MAPPING) the entry.
+	 * IF the entry was empty to begin with:
+	 		return false to signify to our parent node in the recursion,
+	 		that he can terminate the recursion since none of its entries were emptied
+	 * ELSE:
+	 		return true to signify to our next parent that he had one of its entried emptied.
+	 */
+	if (depth == 4 || (*entry == NO_MAPPING) ) {
+		
+		if (depth != 4) { // in this case, there was nothing to remove to begin with
+			return false;
+		} else if (*entry == NO_MAPPING) { // in this case, the entry of the VPN->PPN was empty to begin with
+			return true;
+		} else { // in this case, there was in fact a mapping that VPN held, and we'll nullify it as desired
+			*entry = NO_MAPPING;
+			return true;
+		}
+
+	/* if we still need to advance in the search for the entry, simply advance to the next node in the path */
+	} else { 
+		/* IF the recursion returned that the node that we're at hasn't had any entries emptied:
+		 		we return false to signify to our parent node in the recursion,
+	 			that he can terminate the recursion since none of its entries were emptied
+		 * ELSE
+		 		we continue on to the next step,
+		 		of determining if the node we're at needs to be freed due to any of its entries being emptied,
+		 		which is guaranteed due to <true> being returned.
+		 */
 		if (remove_mapping(entry, vpn, depth + 1) == false) {
 			return false;
 		}
 	}
 
-	if (depth != 0) { // if we aren't in the page table's root node (which shouldn't be freed at all costs), check if the node is empty of mapping, and if so, it frees it, and changes the entry that held the PPN of the node to NO_MAPPING
-		free_node_safe(current_node_ppn);
+	/* if we aren't in the page table's root node (which shouldn't be freed at all costs):
+	 * 		check if the node is empty of mapping:
+	 *			 IF so:
+	 				 it frees it, and changes the entry that held the PPN of the node to NO_MAPPING.
+	 				 Then, we return true to signify to our next parent that he had one of its entried emptied.
+	 * 			 ELSE:
+	 				 the current node wasn't freed, and we return false to signify to our parent node in the recursion,
+	 				 that he can terminate the recursion since none of its entries were emptied. 
+	 */
+	if (depth != 0) {
+		return free_node_safe(current_node_ppn);
 	}
 	
-	return true;
+	return false;
 }
 
 
@@ -123,11 +166,14 @@ static void free_node(uint64_t node_ppn) {
 }
 
 
-static void free_node_safe(uint64_t *node_ppn) {
+static bool free_node_safe(uint64_t *node_ppn) {
 	if (is_node_empty(*node_ppn) == true) {
 		free_node(*node_ppn);
 		*node_ppn = NO_MAPPING;
+		return true;
 	}
+	
+	return false;
 }
 
 /**************** MAIN MECHANISM's DEFINITIONS ******************/
@@ -148,7 +194,6 @@ void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn) {
 
 uint64_t page_table_query(uint64_t pt, uint64_t vpn) {
 	uint64_t current_node_ppn = pt;
-	uint64_t out = NO_MAPPING;
 	
 	for (size_t depth = 0; depth < 5; depth++) { /* Personal calculations showed that the Page Table should be of height 5, with 512 entries for each node */
 		uint64_t* entry = get_entry(vpn, current_node_ppn, depth);
@@ -160,4 +205,6 @@ uint64_t page_table_query(uint64_t pt, uint64_t vpn) {
 			current_node_ppn = *entry;
 		}
 	}
+	
+	return NO_MAPPING;
 }
